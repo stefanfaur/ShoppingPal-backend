b537673ab0c20b60f06931c72a236f9b43617fd4
stefanfaur <stefan.faur@student.upt.ro>
2023-05-23 20:26:32 +0300

    Added python venv to gitignore.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Created FastAPI project. Gotta implement DB now."}
 
 
 @app.get("/hello/{name}")
